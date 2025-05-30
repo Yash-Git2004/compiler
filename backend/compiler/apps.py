e89ffa0f167688e401e92a1688cc6efd7d92b676
GEETANJALI RAWAT <134593959+Geet9337@users.noreply.github.com>
2025-05-30 09:09:43 +0530

    Update apps.py

--- backend/compiler/apps.py
+++ backend/compiler/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 # This class configures the 'compiler' app in the Django project.
 class CompilerConfig(AppConfig):
+    # Specifies the type of primary key to use for models that don’t define one.
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'compiler'
