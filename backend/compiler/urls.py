65a0fcc098b434d69fa67f1b80cc4855dbe69ac9
GEETANJALI RAWAT <134593959+Geet9337@users.noreply.github.com>
2025-05-30 09:21:32 +0530

    Update urls.py

--- backend/compiler/urls.py
+++ backend/compiler/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import CompileCodeView
+from django.urls import path            # Import the path function to define URL routes
+from .views import CompileCodeView      # Import the view that will handle the 'compile' endpoint
 
+# Define URL patterns for this app
 urlpatterns = [
     path('compile/', CompileCodeView.as_view(), name='compile-code'),
 ]
